8dc70940e2a62a46852f6a3c12fa3fa2fd94d42d
Satyam <satyam.goel01@nagarro.com>
2026-05-15 04:13:57 +0530

    Updated the github actions file

---
+++ MonetizeMate-Backend/app/core/config.py
@@ -0,0 +1,21 @@
+import os
+from typing import List
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your-super-secret-key")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost:5432/monetize_mate")
+    # DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://ApiIntegrationPostgresAdmin:PgAdmin%4012345@postgresazureinternal.postgres.database.azure.com:5432/monetize_mate?sslmode=require")
+    UPLOAD_DIRECTORY: str = "uploads"
+    MODEL_STORAGE_DIR: str = "trained_ml_models"
+    GROQ_API_KEY: str = ""  # ← add this line
+    FRONTEND_URL: List[str] = Field(default=["http://localhost:3000"])
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+settings = Settings()
